a3f554fca14b422ebf20f44c58966eea9a257cf1
Patrick Flanagan <valtrain@gmail.com>
2020-03-03 17:52:52 +0100

    Added BigEndianBinaryReader class

---
+++ LibExifCore/BigEndianBinaryReader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace LibExifCore
+{
+    public class BigEndianBinaryReader : BinaryReader
+    {
+        public BigEndianBinaryReader(Stream stream) : base(stream) { }
+
+        public override int ReadInt32()
+        {
+            var data = base.ReadBytes(4);
+            Array.Reverse(data);
+            return BitConverter.ToInt32(data, 0);
+        }
+
+        public override Int16 ReadInt16()
+        {
+            var data = base.ReadBytes(2);
+            Array.Reverse(data);
+            return BitConverter.ToInt16(data, 0);
+        }
+
+        public override Int64 ReadInt64()
+        {
+            var data = base.ReadBytes(8);
+            Array.Reverse(data);
+            return BitConverter.ToInt64(data, 0);
+        }
+
+        public override UInt32 ReadUInt32()
+        {
+            var data = base.ReadBytes(4);
+            Array.Reverse(data);
+            return BitConverter.ToUInt32(data, 0);
+        }
+
+        public override UInt16 ReadUInt16()
+        {
+            var data = base.ReadBytes(2);
+            Array.Reverse(data);
+            return BitConverter.ToUInt16(data, 0);
+        }
+
+        public override UInt64 ReadUInt64()
+        {
+            var data = base.ReadBytes(8);
+            Array.Reverse(data);
+            return BitConverter.ToUInt64(data, 0);
+        }
+    }
+}
