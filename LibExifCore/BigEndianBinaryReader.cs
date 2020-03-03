6b429a0cbee35be9340d4c8423abea071abdc352
Patrick Flanagan <valtrain@gmail.com>
2020-03-03 17:54:51 +0100

    Updated comments

--- LibExifCore/BigEndianBinaryReader.cs
+++ LibExifCore/BigEndianBinaryReader.cs
@@ -3,22 +3,26 @@ using System.IO;
 
 namespace LibExifCore
 {
+    /// <summary>
+    /// BigEndianBinaryReader works the same way as System.IO.BinaryReader, except that it
+    /// reads values as big endian. System.IO.BinaryReader treats all values as little endian.
+    /// </summary>
     public class BigEndianBinaryReader : BinaryReader
     {
         public BigEndianBinaryReader(Stream stream) : base(stream) { }
-
-        public override int ReadInt32()
-        {
-            var data = base.ReadBytes(4);
-            Array.Reverse(data);
-            return BitConverter.ToInt32(data, 0);
-        }
 
         public override Int16 ReadInt16()
         {
             var data = base.ReadBytes(2);
             Array.Reverse(data);
             return BitConverter.ToInt16(data, 0);
+        }
+
+        public override int ReadInt32()
+        {
+            var data = base.ReadBytes(4);
+            Array.Reverse(data);
+            return BitConverter.ToInt32(data, 0);
         }
 
         public override Int64 ReadInt64()
@@ -28,18 +32,18 @@ namespace LibExifCore
             return BitConverter.ToInt64(data, 0);
         }
 
-        public override UInt32 ReadUInt32()
-        {
-            var data = base.ReadBytes(4);
-            Array.Reverse(data);
-            return BitConverter.ToUInt32(data, 0);
-        }
-
         public override UInt16 ReadUInt16()
         {
             var data = base.ReadBytes(2);
             Array.Reverse(data);
             return BitConverter.ToUInt16(data, 0);
+        }
+
+        public override UInt32 ReadUInt32()
+        {
+            var data = base.ReadBytes(4);
+            Array.Reverse(data);
+            return BitConverter.ToUInt32(data, 0);
         }
 
         public override UInt64 ReadUInt64()
